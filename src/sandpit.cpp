66e89206663bff2a36ca373a3a2b18408273417e
Tomos Fearn <tof7@aber.ac.uk>
2023-01-19 22:22:39 +0000

    updated sandbox

--- src/sandpit.cpp
+++ src/sandpit.cpp
@@ -6,6 +6,21 @@
 #include "std_msgs/String.h" //for room name topic
 #include "sensor_msgs/PointCloud2.h"
 
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_ros/transforms.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+
+#include <pcl/conversions.h>
+#include <pcl/point_types.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+
+#include <pcl/filters/extract_indices.h>
+
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -41,16 +56,23 @@ void callback(const sensor_msgs::PointCloud2::ConstPtr& cloud_parameter) {
 
     //get position of point in rectified image array, corresponding with pointcloud
     //int arrayPosition = detectedObjects[isObject].centerY*dpth->row_step + detectedObjects[isObject].centerX*dpth->point_step;
-    double chopSize = (50.0 / 100.0) * double(imageWidth);
+    double chopSize = (20.0 / 100.0) * double(imageWidth);
     double leftChop = chopSize;
     double rightChop = imageWidth - chopSize;
+    double interquartileChop = imageWidth - leftChop - rightChop;
     cout << "chop size " << chopSize << endl;
     cout << "first 20 percent " << leftChop << endl;
     cout << "last 20 percent " << rightChop << endl;
+    cout << "interquartileChop" << interquartileChop << endl;
 
-    //int arrayPosition = detectedObjects[isObject].centerY*dpth->row_step + detectedObjects[isObject].centerX*dpth->point_step;
+    /*//int arrayPosition = detectedObjects[isObject].centerY*dpth->row_step + detectedObjects[isObject].centerX*dpth->point_step;
     for (int imgX = 0; imgX < leftChop; imgX++) { //run from left to right of first chop
         for (int imgY = 0; imgY < imageHeight; imgY++) {
+
+            newCloud.height = row;
+            newCloud.width = col;
+            newCloud.points.resize(cloud.height * cloud.width);
+
             int arrayPosition = imgY * newCloud.row_step + imgX * newCloud.point_step;
 
             int arrayPosX = arrayPosition + newCloud.fields[0].offset; // X has an offset of 0
@@ -61,12 +83,73 @@ void callback(const sensor_msgs::PointCloud2::ConstPtr& cloud_parameter) {
             memcpy(&Y, &newCloud.data[arrayPosY], sizeof(float)); //add value from depth point to Y
             memcpy(&Z, &newCloud.data[arrayPosZ], sizeof(float)); //add value from depth point to Z
 
-            newCloud.data[arrayPosX] = 0;
-            newCloud.data[arrayPosY] = 0;
-            newCloud.data[arrayPosZ] = 0;
+            newCloud.data[arrayPosX] = -1;
+            newCloud.data[arrayPosY] = -1;
+            newCloud.data[arrayPosZ] = -1;
         }
-    }
-    pub_ptr->publish(newCloud);
+    }*/
+    //pub_ptr->publish(newCloud);
+
+
+
+
+
+/*
+float depthThreshold = 0.5;
+float threshold2 = depthThreshold*depthThreshold;
+
+// Container for original & filtered data
+pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2; 
+pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
+pcl::PCLPointCloud2 cloud_filtered;
+
+// Convert to PCL data type
+pcl_conversions::toPCL(*cloud_parameter, *cloud);
+
+// Perform the actual filtering
+for (int p=0; p<cloud->points.size(); ++p)
+{
+// find the squared distance from the origin.
+float pointDepth2 = (cloud->points[p].x * cloud->points[p].x) +
+                    (cloud->points[p].y * cloud->points[p].y) + 
+                    (cloud->points[p].z * cloud->points[p].z));
+
+// remove point if it's within the threshold range
+if (pointDepth2 < threshold2)
+{
+    cloud->points[p] = cloud->points[cloud->points.size()-1];
+    cloud->points.resize(cloud->points.size()-1);
+    --p;
+}
+}
+
+pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+sor.setInputCloud (cloudPtr);
+sor.setLeafSize (0.1, 0.1, 0.1);
+sor.filter (cloud_filtered);
+
+// Convert to ROS data type
+sensor_msgs::PointCloud2 output;
+pcl_conversions::fromPCL(cloud_filtered, output);
+
+// Publish the data
+pub.publish (output);
+*/
+
+
+
+pcl::PointCloud<pcl::PointXYZRGB> cloud_in; //original pc as pcl version
+
+pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
+
+pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+
+//STEP 0 Convert sensor_msgs to pcl
+pcl::fromROSMsg(newCloud, cloud_in);
+
+int i = 0, nr_points = (int) cloud_in.size ();
+//cout << nr_points << endl;
+
 }
 
 /**
